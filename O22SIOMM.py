f1ebf0e71154f27336f6361c9d8c5466c4eb0f10
Terry Orchard <torchard@opto22.com>
2018-08-24 18:00:03 -0700

    PEP-8 line width

--- O22SIOMM.py
+++ O22SIOMM.py
@@ -107,47 +107,57 @@ class O22MMP:
         if (index < 0 or index > O22SIOUT.MAX_ELEMENTS_INTEGER):
             return 'index out of bounds'
         offset = O22SIOUT.BASE_SCRATCHPAD_INTEGER + (index * 0x04)
-        return int(self.UnpackReadResponse(self.ReadBlock(offset, 4), 'i'))
+        data = self.ReadBlock(offset, 4)
+        return int(self.UnpackReadResponse(data, 'i'))
 # ScratchPad integer write
     def SetScratchPadIntegerArea(self, index, value):
         if (index < 0 or index > O22SIOUT.MAX_ELEMENTS_INTEGER):
             return 'index out of bounds'
         offset = O22SIOUT.BASE_SCRATCHPAD_INTEGER + (index * 0x04)
-        return self.UnpackWriteResponse(self.WriteBlock(offset, self.PackInteger(value)))
+        data = self.WriteBlock(offset, self.PackInteger(value))
+        return self.UnpackWriteResponse(data)
 ## FLOATS
 # ScratchPad float read
     def GetScratchPadFloatArea(self, index):
         if (index > O22SIOUT.MAX_ELEMENTS_FLOAT):
             return 'index out of bounds'
         offset = O22SIOUT.BASE_SCRATCHPAD_FLOAT + (index * 0x04)
-        return float(self.UnpackReadResponse(self.ReadBlock(offset, 4), 'f'))
+        data = self.ReadBlock(offset, 4)
+        return float(self.UnpackReadResponse(data, 'f'))
 # ScratchPad float write
     def SetScratchPadFloatArea(self, index, value):
         if (index > O22SIOUT.MAX_ELEMENTS_FLOAT):
             return 'index out of bounds'
         offset = O22SIOUT.BASE_SCRATCHPAD_FLOAT + (index * 0x04)
-        return self.UnpackWriteResponse(self.WriteBlock(offset, self.PackFloat(value)))
+        data = self.WriteBlock(offset, self.PackFloat(value))
+        return self.UnpackWriteResponse(data)
 ## STRINGS
 # ScratchPad string read
     def GetScratchPadStringArea(self, index):
-        if (index * O22SIOUT.OFFSET_SCRATCHPAD_STRING) >= O22SIOUT.MAX_BYTES_STRING or index < 0:
+        loc = index * O22SIOUT.OFFSET_SCRATCHPAD_STRING
+        if (loc >= O22SIOUT.MAX_BYTES_STRING) or (index < 0):
             return 'index out of bounds'
-        offset = O22SIOUT.BASE_SCRATCHPAD_STRING + (index * O22SIOUT.OFFSET_SCRATCHPAD_STRING)
-        sizeData = self.UnpackReadResponse(self.ReadBlock(offset+0x01, 1), 'c')[1:-1]
-        # sizeData resolves to an ASCII character `c` *or* a hex `/x00` number. Convert to int...
-        size = ord(sizeData) if len(sizeData)==1 else int('0'+sizeData[1:], 16)
-        # Note: string size is at offset, string data is at offset + 2 bytes
-        return self.UnpackReadResponse(self.ReadBlock(offset+0x02, size), 'NONE')
+        offset = O22SIOUT.BASE_SCRATCHPAD_STRING
+                + (index * O22SIOUT.OFFSET_SCRATCHPAD_STRING)
+        sizeData = 
+        rawSize = self.UnpackReadResponse(sizeData, 'c')[1:-1]
+        # rawSize is char `c` *or* a hex `/x00` number. Convert to int:
+        size = ord(rawSize) if len(rawSize)==1 else int('0'+rawSize[1:], 16)
+        # Note: string size is at offset, string data is at offset + 2
+        data = self.ReadBlock(offset+0x02, size)
+        return self.UnpackReadResponse(data, 'NONE')
 # ScratchPad string write
-    def SetScratchPadStringArea(self, index, data):
-        if (len(data) > 127): return 'string must be < 128 characters'
-        if (index * O22SIOUT.OFFSET_SCRATCHPAD_STRING) < O22SIOUT.MAX_BYTES_STRING:
-            # Note: string size is at offset, string data is at offset + 2 bytes
-            offset = O22SIOUT.BASE_SCRATCHPAD_STRING + (index * O22SIOUT.OFFSET_SCRATCHPAD_STRING) + 0x02
+    def SetScratchPadStringArea(self, index, value):
+        if (len(value) > 127): return 'string must be < 128 characters'
+        loc = index * O22SIOUT.OFFSET_SCRATCHPAD_STRING
+        if loc < O22SIOUT.MAX_BYTES_STRING:
+            offset = O22SIOUT.BASE_SCRATCHPAD_STRING
+                    + (index * O22SIOUT.OFFSET_SCRATCHPAD_STRING) + 0x02
             hexvals = []
-            for i in range(len(data)):
-                hexvals.append(ord(data[i]))
-            return self.UnpackWriteResponse(self.WriteBlock(offset, hexvals))
+            for i in range(len(value)):
+                hexvals.append(ord(value[i]))
+            data = self.WriteBlock(offset, hexvals)
+            return self.UnpackWriteResponse()
         else: return 'index out of bounds'
 
 
@@ -157,41 +167,52 @@ class O22MMP:
     def UnpackReadResponse(self, data, data_type):
         data_block = data[16:]
         output = ''
-        # unpack data and build firmware version string
-        #       version comes in [V, v, N, c] HEX format, output "NV.vc" string.
-        #       Replace `N` with A, B, R, S or ?, Replace `c` with 0 = 'a', 1 = 'b', ...
+        # unpack data and build firmware version string "NV.vc"
+        #       version comes in [V, v, N, c] HEX format
+        #       Replace `N` with A, B, R, S or ?
         if data_type == 'FIRMWARE':
             version = []
             for i in range(4):
-                nextChar = str(struct.unpack_from('>c', bytearray(data_block[i])))[4:-3]
+                raw = struct.unpack_from('>c', bytearray(data_block[i]))
+                nextChar = str(raw)[4:-3]
                 version.append(nextChar)
             if   int(version[2]) == 0:  output = 'A'
             elif int(version[2]) == 1:  output = 'B'
             elif int(version[2]) == 2:  output = 'R'
             elif int(version[2]) == 3:  output = 'S'
             else:                       output = '?'
-            output += str(int(version[0])) + '.' + str(int(version[1])) + chr(int(version[3])+97)
+            # Replace `c` with 0 = 'a', 1 = 'b', ... using chr(..+97)
+            output += str(int(version[0])) + '.'
+                    + str(int(version[1])) + chr(int(version[3])+97)
         # unpack data and format as an IP address
         elif data_type == 'IP':
-            for i in range(len(data_block)): # *trim first 3 and last 3 around <data>: ('/<data>',)
-                nextChar = str(struct.unpack_from('>c', bytearray(data_block[i])))[3:-3]
-                if(nextChar == 'n'): nextChar = 10 # catch:  10  =(hex)=>  0x0A  =(ascii)=>  \n  =(trim)=>  'n'
-                else: nextChar = int('0'+nextChar, 16) # convert hex to decimal
+            for i in range(len(data_block)):
+                raw = struct.unpack_from('>c', bytearray(data_block[i]))
+                # *trim first 3 and last 3 around <data>: ('/<data>',)
+                nextChar = str(raw)[3:-3]
+                # catch:  10  =(hex)=>  0x0A  =(ascii)=>  \n  =(trim)=>  'n'
+                if(nextChar == 'n'): nextChar = 10
+                else: nextChar = int('0'+nextChar, 16) # hex -> decimal
                 output += str(nextChar) + '.'
-            output = output[:-1] # trim trailing .
+            output = output[:-1] # trim trailing '.'
         # unpack data and format as a MAC address
         elif data_type == 'MAC':
-            for i in range(len(data_block)): # trim first 2 last 3 around <data>: ('<data>',)
-                nextChar = str(struct.unpack_from('>c', bytearray(data_block[i])))[2:-3]
-                if(len(nextChar) == 1): nextChar = hex(ord(nextChar)) # force valid ascii back into hex
+            for i in range(len(data_block)):
+                # trim first 2 last 3 around <data>: ('<data>',)
+                raw = struct.unpack_from('>c', bytearray(data_block[i]))
+                nextChar = str(raw)[2:-3]
+                # force valid ascii (single-character) back into hex
+                if(len(nextChar) == 1): nextChar = hex(ord(nextChar))
                 output += nextChar[2:] + '-' # trim 0x, and dash
-            output = output[:-1].upper() # trim out trailing dash, force upper case
+            output = output[:-1].upper() # trim trailing '-'
         # unpack data that has no formatting
         elif data_type == 'NONE':
             output = data_block
-        # unpack data of a specific given struct data_type (c, i, f, l, q, etc.) *convert type after calling the function*
+        # unpack data of a specific given struct data_type (c, i, f, etc.)
+        #       *convert type after calling the function*
         else:
-            output = str(struct.unpack_from('>'+data_type, bytearray(data_block)))[1:-2]
+            raw = struct.unpack_from('>'+data_type, bytearray(data_block))
+            output = str(raw)[1:-2]
         return output
 ## UnpackWriteResponse
     def UnpackWriteResponse(self, data):
@@ -217,10 +238,12 @@ class O22MMP:
         if(value > 255):
             hexvals = [0, 0, 0, 0]
             value = hex(value)
-            # odd-length hex values (ex. 0x1FC) need to be padded to even length (ex 0x01FC)
-            value = str(value)[2:] if(len(str(value)) % 2 == 0) else ('0'+str(value)[2:])
-            for i in range(len(value)/2): # split value into bytes: each max. 0xFF
-                hexvals[i+(4-len(value)/2)] = int(str(value)[(2*i):(2*i)+2], 16)
+            evenLength = (len(str(value)) % 2 == 0)
+            # force all values to have an even length for byte-wise splitting:
+            value = str(value)[2:] if(evenLength) else ('0'+str(value)[2:])
+            for i in range(len(value)/2): # split value into max. 0xFF bytes
+                thisPart = int(str(value)[(2*i):(2*i)+2], 16)
+                hexvals[i+(4-len(value)/2)] = thisPart
         return hexvals
 
 
@@ -242,14 +265,22 @@ class O22MMP:
 ## BuildReadBlockRequest
     def BuildReadBlockRequest(self, dest, size):
         tcode = O22SIOUT.TCODE_READ_BLOCK_REQUEST
-        block = [0, 0, (self.tlabel << 2), (tcode << 4), 0, 0, 255, 255]
-        block += [int(str(hex(dest))[2:4],16), int(str(hex(dest))[4:6],16), int(str(hex(dest))[6:8],16), int(str(hex(dest))[8:10],16), 0,size, 0,0]
+        block = [
+            0, 0, (self.tlabel << 2), (tcode << 4), 0, 0, 255, 255,
+            int(str(hex(dest))[2:4],16), int(str(hex(dest))[4:6],16),
+            int(str(hex(dest))[6:8],16), int(str(hex(dest))[8:10],16),
+            0,size, 0,0
+            ]
         return bytearray(block)
 ## BuildWriteBlockRequest
     def BuildWriteBlockRequest(self, dest, data):
         tcode = O22SIOUT.TCODE_WRITE_BLOCK_REQUEST
-        block = [0, 0, (self.tlabel << 2), (tcode << 4), 0, 0, 255, 255]
-        block += [int(str(hex(dest))[2:4],16), int(str(hex(dest))[4:6],16), int(str(hex(dest))[6:8],16), int(str(hex(dest))[8:10],16), 0,len(data), 0,0]
+        block = [
+            0, 0, (self.tlabel << 2), (tcode << 4), 0, 0, 255, 255,
+            int(str(hex(dest))[2:4],16), int(str(hex(dest))[4:6],16),
+            int(str(hex(dest))[6:8],16), int(str(hex(dest))[8:10],16),
+            0,len(data), 0,0
+            ]
         return bytearray(block+data)
 
 ## CLOSE SOCKET / END SESSION
